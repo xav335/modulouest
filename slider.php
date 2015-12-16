b39dd580bfa99889c67bf97092b68a27a8b85ca9
Xavier Gonzalez <xavier@gonzalez.pm>
2015-12-16 14:29:36 +0100

    video online

--- slider.php
+++ slider.php
@@ -12,51 +12,14 @@
 				            &amp; aménagement<br/>
 				            <span>de véhicules utilitaires</span>
 			            </div>
-			            <a href="https://youtu.be/f65EwG10kgY?autoplay=1" class="play">
+			            <a href="https://www.youtube.com/embed/T9CSZb46ahg?autoplay=1" class="play">
 				            <span>Modul-Ouest</span>
-				            #1 En vidéo
+				            En vidéo
 			            </a>
 		            </div>
 	            </div>
             </div>
             
-            <div class="swiper-slide">
-	            <div class="row">
-		            <div class="large-6 medium-6 small-6 columns">
-			            <img src="img/camion-01.png" alt="" />
-					</div>
-		            <div class="large-6 medium-6 small-6 columns">
-			            <div class="text-slider">
-				            équipement<br/>
-				            &amp; aménagement<br/>
-				            <span>de véhicules utilitaires</span>
-			            </div>
-			            <a href="#" class="play">
-				            <span>Modul-Ouest</span>
-				            #2 Pas de vidéo...
-			            </a>
-		            </div>
-	            </div>
-            </div>
-            
-            <div class="swiper-slide">
-	            <div class="row">
-		            <div class="large-6 medium-6 small-6 columns">
-			            <img src="img/camion-01.png" alt="" />
-					</div>
-		            <div class="large-6 medium-6 small-6 columns">
-			            <div class="text-slider">
-				            équipement<br/>
-				            &amp; aménagement<br/>
-				            <span>de véhicules utilitaires</span>
-			            </div>
-			            <a href="#" class="play">
-				            <span>Modul-Ouest</span>
-				            #3 Une nouvelle vidéo
-			            </a>
-		            </div>
-	            </div>
-            </div>
-            
+             
         </div>
 	</div>
