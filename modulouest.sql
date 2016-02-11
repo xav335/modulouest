528d7e34fa8cb0c1344638183499e80388320c21
Xavier Gonzalez <jgonzalez@iconeo.fr>
2016-02-12 00:45:30 +0100

    AFTER ATTACK + DB

--- modulouest.sql
+++ modulouest.sql
@@ -89,7 +89,7 @@ CREATE TABLE `contact` (
   `fromgoldbook` tinyint(4) NOT NULL DEFAULT '0',
   `fromcontact` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +98,7 @@ CREATE TABLE `contact` (
 
 LOCK TABLES `contact` WRITE;
 /*!40000 ALTER TABLE `contact` DISABLE KEYS */;
-INSERT INTO `contact` VALUES (2,'q','q','','','','franck_langleron@hotmail.com','q','',1,1,1);
+INSERT INTO `contact` VALUES (2,'q','q','','','','franck_langleron@hotmail.com','q','',1,1,1),(3,'Javier','Alvarez','55 rue anatole france','33150','Latresne','xavier@gonzalez.pm','06 81 73 18 70','mon message de test',0,0,1),(4,'Xavier','Gonzalez','','','','xavi@gmaeil.cm','05050505','Test',0,0,1);
 /*!40000 ALTER TABLE `contact` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,7 +139,7 @@ CREATE TABLE `goldbook` (
   `message` text,
   `online` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -193,7 +193,7 @@ CREATE TABLE `news` (
   `image1` varchar(250) DEFAULT NULL,
   `online` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_news`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -202,7 +202,7 @@ CREATE TABLE `news` (
 
 LOCK TABLES `news` WRITE;
 /*!40000 ALTER TABLE `news` DISABLE KEYS */;
-INSERT INTO `news` VALUES (30,'2015-11-07 00:00:00','Nouveaux produits','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.','/_MG_5255-30.jpg',1),(31,'2015-11-22 00:00:00','L\'autre jour...',' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pellentesque ut risus id aliquam. Duis dapibus rhoncus neque ac tempus. Nunc laoreet tincidunt libero, a fermentum lacus semper ac. Donec lobortis pretium urna euismod auctor. Duis ut mattis metus, ac ultricies eros. Etiam a porta purus, efficitur pulvinar nibh. Mauris a rutrum quam. In arcu leo, egestas vitae mi a, viverra finibus mauris. Nunc consectetur tellus at ligula pulvinar sodales efficitur quis est. Proin porttitor massa mauris, ut vulputate nulla malesuada sed. Mauris fermentum fermentum rhoncus. Nullam convallis at enim at placerat. Nulla ut augue tincidunt diam egestas luctus. Sed orci justo, pellentesque vel aliquet non, ornare id est. Donec dui sapien, varius eu lectus at, ullamcorper ornare nunc. Quisque a sollicitudin metus.\r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non blandit ligula, eu congue elit. Etiam tempor tortor non ante placerat placerat. Quisque convallis porttitor nisi, nec euismod velit sollicitudin at. Duis pretium ex sed enim consectetur, eu egestas diam vestibulum. Aliquam vulputate turpis et risus congue interdum. Fusce molestie, magna vel interdum volutpat, purus est lobortis erat, eget interdum lorem massa sed risus. Morbi justo dolor, convallis eget cursus eu, luctus eu lectus. Vestibulum mi risus, condimentum a nibh vel, imperdiet consectetur purus. Praesent vel ante erat. In hac habitasse platea dictumst. Aenean pellentesque enim nisi, vel varius justo fringilla in. Vestibulum scelerisque pretium dolor, vel pretium metus dapibus sed. Aenean gravida, erat quis tempus pulvinar, ligula odio imperdiet velit, in semper leo augue id neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;\r\n\r\nNam nec odio ut erat rutrum tempor. Etiam luctus, ante nec sodales lobortis, ipsum sapien ultricies orci, nec blandit odio nunc in ligula. Mauris a egestas turpis, a tristique libero. Proin aliquam ex ac feugiat pellentesque. Aliquam ac nibh sit amet nunc sodales tincidunt. Aenean nec facilisis tortor, porta iaculis dui. Aliquam id iaculis purus. Praesent id mi sed turpis eleifend interdum. Pellentesque ac fringilla ex. ','/IMG_6229-.jpg',1);
+INSERT INTO `news` VALUES (31,'2016-01-26 00:00:00','Découvrez notre nouveau site internet',' ','/Screen_Shot_2016_01_30_at_16.58.02-31.png',1);
 /*!40000 ALTER TABLE `news` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -285,7 +285,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (1,96,'Aménagement bois ','L\'aménagement ou l\'équipement d\'un véhicule utilitaire est souvent considéré comme une charge supplémentaire, alors qu\'au contraire un véhicule correctement aménagé est un gain de productif direct.   \r\nGain de temps: Plusieurs études menées faisant apparaitre qu\'un véhicule correctement aménagé permettait de gagner en moyenne une heure par jour. L\'investissement de départ est alors rapidement amorti. \r\nImage de votre entreprise : Un véhicule mal améngé donne une image négative de votre entreprise , comment faire confiance à un professionnel dont le véhicule est un amoncellement d\'outils, de matériaux .... un client se demande rapidement dans quel état il retrouvera son chantier. \r\nSécurité : Moins de risques d\'avoir des objets mobiles à l\'intérieur de votre véhicule , moins de riques de tranferts de charges inopinées risquant de déstabiliser votre véhicule.  \r\n\r\nTous nos aménagements bois sont réalisés sur-mesure en fonction de vos besoins et votre souhait. \r\nRéalisation en contreplaqué brut ou filmé, selon votre choix ?  \r\n \r\n\r\n   ','/test-1.pdf','1','1'),(3,0,'Aménagement spécifique','Depuis plus de 20 ans. MODUL-OUEST répond à la demande de ses clients, nous sommes en mesure d\'apporter une solution à vos contraintes : \r\nAluminium, Résine, Kit Cover ... du plus simple au plus complexe.    ','','1','1'),(6,0,'Signalisation lumineuse et balisage  ','Nous équipons tous types de véhicules de POLICE , GENDARMERIE  et BTP . \r\nBalisage classe A et B , Gyrophare à leds , Triangle a relevage manuel ou électrique .....\r\nNous commercialisons les produits: SIRAC. MERCURA. AXIMUM. T2S . SARR ....    ','','0','0'),(7,0,'Habillage du véhicule   ','L\'habillage d\'un véhicule , consiste à protéger l\'intérieur de chargement de votre véhicule utilitaire des chocs liés à son utilisation.\r\nNos protections sont réalisées en contre plaqué de qualité oukoumé en 5 ou 8 mm d\'épaisseur suivant le type de véhicule .\r\nLes planchers sont réalisés en contre plaqué brut de 15mm ou antidérapant 12 mm suivant la demande et l\'utilisation. \r\nFinition: Joints silicone et barre de seuil aluminium.     ','','0','0'),(8,0,'Signalisation lumineuse ','Partenaire de la marque SIRAC , nous équipons les véhicules de POLICE, GENDARMERIE et POMPIERS ','','0','0'),(9,58,' Aménagement modulaire ','System Edstrom fondée en 1958 , est l\'une des plus ancienne société en EUROPE fabriquant et commercialisant des aménagements pour les véhicules utilitaires. Plus de 50 ans d\'expérience dans le développement de nos produits , plus de 50 ans d\'écoute attentive des souhaits de nos clients pour trouver des solutions pour les utilisateurs. \r\nNotre priorité la sécurité, nos aménagements ont obtenus la certification française INRS NS286     ','','1','1'),(10,98,'Protection aluminium','Une protection aluminium vous permet un lavage ou une désinfection de votre véhicule  ','','1','1'),(11,96,'Habillage bois ','Nos habillage sont réalisés en contreplaqué de qualité supérieur, nous protégeons les parois , les portes et le plancher, ce dernier peut être en contreplaqué antidérapant .\r\nNous pouvons vous proposer du CP filmé ou du Poly pro.    ','','0','0'),(12,58,'Aménagement modulaire System Edstrom ','Véhicule atelier ','','1','1');
+INSERT INTO `product` VALUES (1,96,'Aménagement bois ','L\'aménagement ou l\'équipement d\'un véhicule utilitaire est souvent considéré comme une charge supplémentaire, alors qu\'au contraire un véhicule correctement aménagé est un gain de productif direct.   \r\nGain de temps: Plusieurs études menées faisant apparaitre qu\'un véhicule correctement aménagé permettait de gagner en moyenne une heure par jour. L\'investissement de départ est alors rapidement amorti. \r\nImage de votre entreprise : Un véhicule mal améngé donne une image négative de votre entreprise , comment faire confiance à un professionnel dont le véhicule est un amoncellement d\'outils, de matériaux .... un client se demande rapidement dans quel état il retrouvera son chantier. \r\nSécurité : Moins de risques d\'avoir des objets mobiles à l\'intérieur de votre véhicule , moins de riques de tranferts de charges inopinées risquant de déstabiliser votre véhicule.  \r\n\r\nTous nos aménagements bois sont réalisés sur-mesure en fonction de vos besoins et votre souhait. \r\nRéalisation en contreplaqué brut ou filmé, selon votre choix ?  \r\n \r\n\r\n   ','/0115_002-1.jpg','0','1'),(3,0,'Aménagement spécifique','Depuis plus de 20 ans. MODUL-OUEST répond à la demande de ses clients, nous sommes en mesure d\'apporter une solution à vos contraintes : \r\nAluminium, Résine, Kit Cover ... du plus simple au plus complexe.    ','','0','1'),(6,0,'Signalisation lumineuse et balisage  ','Nous équipons tous types de véhicules de POLICE , GENDARMERIE  et BTP . \r\nBalisage classe A et B , Gyrophare à leds , Triangle a relevage manuel ou électrique .....\r\nNous commercialisons les produits: SIRAC. MERCURA. AXIMUM. T2S . SARR ....    ','','0','0'),(7,0,'Habillage du véhicule   ','L\'habillage d\'un véhicule , consiste à protéger l\'intérieur de chargement de votre véhicule utilitaire des chocs liés à son utilisation.\r\nNos protections sont réalisées en contre plaqué de qualité oukoumé en 5 ou 8 mm d\'épaisseur suivant le type de véhicule .\r\nLes planchers sont réalisés en contre plaqué brut de 15mm ou antidérapant 12 mm suivant la demande et l\'utilisation. \r\nFinition: Joints silicone et barre de seuil aluminium.     ','','0','0'),(8,0,'Signalisation lumineuse ','Partenaire de la marque SIRAC , nous équipons les véhicules de POLICE, GENDARMERIE et POMPIERS ','','0','0'),(9,58,' Aménagement modulaire ','System Edstrom fondée en 1958 , est l\'une des plus ancienne société en EUROPE fabriquant et commercialisant des aménagements pour les véhicules utilitaires. Plus de 50 ans d\'expérience dans le développement de nos produits , plus de 50 ans d\'écoute attentive des souhaits de nos clients pour trouver des solutions pour les utilisateurs. \r\nNotre priorité la sécurité, nos aménagements ont obtenus la certification française INRS NS286     ','','1','1'),(10,98,'Protection aluminium','Une protection aluminium vous permet un lavage ou une désinfection de votre véhicule  ','','0','1'),(11,96,'Habillage bois ','Nos habillage sont réalisés en contreplaqué de qualité supérieur, nous protégeons les parois , les portes et le plancher, ce dernier peut être en contreplaqué antidérapant .\r\nNous pouvons vous proposer du CP filmé ou du Poly pro.    ','','0','0'),(12,58,'Aménagement modulaire System Edstrom ','Véhicule atelier ','','0','1');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -302,7 +302,7 @@ CREATE TABLE `product_image` (
   `fichier` varchar(100) NOT NULL,
   `defaut` enum('oui','non') NOT NULL DEFAULT 'non',
   PRIMARY KEY (`num_image`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -311,7 +311,7 @@ CREATE TABLE `product_image` (
 
 LOCK TABLES `product_image` WRITE;
 /*!40000 ALTER TABLE `product_image` DISABLE KEYS */;
-INSERT INTO `product_image` VALUES (20,4,'/IMG_6130-4.jpg','non'),(25,4,'/IMG_6686-4.jpg','oui'),(30,9,'/Fevrier_14_001-9.jpg','oui'),(31,9,'/Fevrier_14_004-9.jpg','non'),(32,9,'/Samsung_1115_004-9.jpg','non'),(33,10,'/003-10.jpg','oui'),(34,11,'/0115_003-11.jpg','oui'),(35,6,'/Oct_15_Tel_037-6.jpg','oui'),(36,3,'/Oct_15_042-3.jpg','oui'),(37,12,'/Samsung_1115_004-12.jpg','oui');
+INSERT INTO `product_image` VALUES (20,4,'/IMG_6130-4.jpg','non'),(25,4,'/IMG_6686-4.jpg','oui'),(30,9,'/Fevrier_14_001-9.jpg','oui'),(31,9,'/Fevrier_14_004-9.jpg','non'),(32,9,'/Samsung_1115_004-9.jpg','non'),(33,10,'/003-10.jpg','oui'),(34,11,'/0115_003-11.jpg','oui'),(35,6,'/Oct_15_Tel_037-6.jpg','oui'),(36,3,'/Oct_15_042-3.jpg','oui'),(37,12,'/Samsung_1115_004-12.jpg','oui'),(38,1,'/0115_002-1.jpg','oui');
 /*!40000 ALTER TABLE `product_image` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -324,4 +324,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-01-21 12:25:48
+-- Dump completed on 2016-02-12  0:44:51
