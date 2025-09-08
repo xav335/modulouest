08394ef39a1eda2b6128174960e764aa9935377d
Jgonzalez <fjavi.gonzalez@gmail.com>
2025-09-08 17:07:09 +0200

    Avant modifs NV client

--- admin/inc-menu.php
+++ admin/inc-menu.php
@@ -6,7 +6,7 @@
 	                        <span class="icon-bar"></span>
 	                        <span class="icon-bar"></span>
 	                    </button>
-                    <a class="navbar-brand" href="/admin/"><img src="/admin/img/xlogo.png" width="150" alt="Back-Office"/></a>
+                    <a class="navbar-brand" href="/admin/"><img src="../img/marker.png" width="50" alt="Back-Office"/></a>
                 	</div>
                 	<div class="col-md-6 collapse navbar-collapse">
 	           			<ul class="nav navbar-nav">
@@ -33,7 +33,7 @@
 					            	<li><a href="/admin/livre_dor/liste.php">Modif / Suppr</a></li>
 					          	</ul>
 					        </li>
-					         <li class="dropdown">
+					         <!-- li class="dropdown">
 					          	<a href="contact-list.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Contacts <span class="caret"></span></a>
 					          	<ul class="dropdown-menu" role="menu">
 					            	<li><a href="/admin/contact-edit.php">Ajout</a></li>
@@ -41,8 +41,8 @@
 					            	<li class="divider"></li>
 					            	<li><a href="/admin/contact-import.php">Import / Export</a></li>
 					          	</ul>
-					        </li>
-					         <li class="dropdown">
+					        </li -->
+					         <!-- li class="dropdown">
 					          	<a href="newsletter-list.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Newsletter<span class="caret"></span></a>
 					          	<ul class="dropdown-menu" role="menu">
 					            	<li><a href="/admin/newsletter-edit.php">Ajout Newsletter</a></li>
@@ -50,7 +50,7 @@
 					            	<li class="divider"></li>
 					            	<li><a href="/admin/newsletterjournal-list.php">Journal des envoi</a></li>
 					          	</ul>
-					        </li>
+					        </li -->
 							<!--<li class="dropdown">
 					          	<a href="bdc.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Bon de commande <span class="caret"></span></a>
 					          	<ul class="dropdown-menu" role="menu">
